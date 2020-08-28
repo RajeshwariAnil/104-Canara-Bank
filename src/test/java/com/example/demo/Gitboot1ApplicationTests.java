ad43cd353d88ae7266a3018df5fa93cf3f2bf670
rajeshwarianil <rajeshwari0494@gmail.com>
2020-08-28 12:42:08 +0530

    project addad

---
+++ src/test/java/com/example/demo/Gitboot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gitboot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
